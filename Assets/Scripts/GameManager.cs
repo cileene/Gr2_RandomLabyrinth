86608859d36b458e83ebcfbdac7c8a0d675f246f
cileene <106276740+cileene@users.noreply.github.com>
2025-05-01 00:40:05 +0200

    Refactor GameManager to implement Singleton pattern and enhance game state management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,55 +1,85 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//TODO: Add time tracking
-//TODO: Add Singleton pattern
-//TODO: Make serilaizable
+[Serializable]
 public class GameManager : MonoBehaviour
 {
-    public enum GameState { Start, Playing, Won, Lost }
-    public GameState currentState;
+    public static GameManager Instance { get; private set; }
 
-    void Start()
+    // ------------------ VARIABLES ------------------
+    [Header("State")]
+    public GameState currentState;
+    public enum GameState { Start, Phase1, Phase2, Won, Lost }
+    
+    [Header("Progression")]
+    public int currentLevel;
+    public int highScore;
+    public float lastRunTime;
+   
+    private float _currentRunTime;
+    
+    
+    // ------------------ METHODS ------------------
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+        
+    }
+
+    private void Start()
     {
         string currentScene = SceneManager.GetActiveScene().name;
 
-        if (currentScene == "StartScene")
-        {
-            currentState = GameState.Start;
-        }
-        else if (currentScene == "PCGTestScene")
-        {
-            currentState = GameState.Playing;
-        }
+        if (currentScene == "StartScene") currentState = GameState.Start;
+        else if (currentScene == "PCGTestScene") currentState = GameState.Phase1;
     }
 
-    void Update()
+    private void Update()
     {
-        if (currentState == GameState.Start && Input.anyKeyDown)
-        {
-            StartGame();
-        }
+        WaitForKeyToStart();
+        IncrementTime();
+    }
+
+    private void WaitForKeyToStart()
+    {
+        if (currentState == GameState.Start && Input.anyKeyDown) StartGame();
+    }
+
+    private void IncrementTime()
+    {
+        if (currentState is GameState.Phase1 or GameState.Phase2) _currentRunTime += Time.deltaTime;
     }
 
     public void StartGame()
     {
-        currentState = GameState.Playing;
-        SceneManager.LoadScene("PCGTestScene");
+        _currentRunTime = 0f;
+        currentState = GameState.Phase1;
+        SceneManager.LoadSceneAsync("PCGTestScene");
     }
 
-    public void WinGame()
+    public void WinLevel()
     {
-        if (currentState != GameState.Playing) return;
+        if (currentState != GameState.Phase2) return;
+        currentLevel++;
 
         currentState = GameState.Won;
-        SceneManager.LoadScene("WinScene");
+        SceneManager.LoadSceneAsync("WinScene");
     }
 
     public void LoseGame()
     {
-        if (currentState != GameState.Playing) return;
-
+        if (currentState is not (GameState.Phase1 or GameState.Phase2)) return;
         currentState = GameState.Lost;
-        SceneManager.LoadScene("LoseScene");
+        
+        lastRunTime = _currentRunTime;
+        if (currentLevel > highScore) highScore = currentLevel;
+
+        SceneManager.LoadSceneAsync("LoseScene");
     }
 }
