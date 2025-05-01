9b5310f0b1f510e35306db70193409a84f3b5b2a
ValdemarWessing <iq62FM@student.aau.dk>
2025-05-01 09:25:57 +0200

    Revert "Merge branch 'State-Patterns' into test-PCG"
    This reverts commit 3716a765f886ec21f3b493a5ef0c17a066ab3b9d, reversing
    changes made to 207db4a3e42dc97492625c7cff89fbce55ee797b.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,83 +1,54 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[Serializable]
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-
-    // ------------------ VARIABLES ------------------
-    [Header("State")]
+    public enum GameState { Start, Playing, Won, Lost }
     public GameState currentState;
-    public enum GameState { Start, Phase1, Phase2, Won, Lost }
-    
-    [Header("Progression")]
-    public int currentScore;
-    public int highScore;
-    public float lastRunTime;
-   
-    private float _currentRunTime;
-    
-    
-    // ------------------ METHODS ------------------
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-        
-    }
 
-    private void Start()
+    void Start()
     {
         string currentScene = SceneManager.GetActiveScene().name;
 
-        if (currentScene == "StartScene") currentState = GameState.Start;
-        else if (currentScene == "Labyrinth") currentState = GameState.Phase1;
+        if (currentScene == "StartScene")
+        {
+            currentState = GameState.Start;
+        }
+        else if (currentScene == "PCGTestScene")
+        {
+            currentState = GameState.Playing;
+        }
     }
 
-    private void Update()
+    void Update()
     {
-        if (currentState == GameState.Start && Input.anyKeyDown) StartGame();
-        if (currentState is GameState.Phase1 or GameState.Phase2) _currentRunTime += Time.deltaTime;
+        if (currentState == GameState.Start && Input.anyKeyDown)
+        {
+            StartGame();
+        }
     }
 
     public void StartGame()
     {
-        _currentRunTime = 0f;
-        currentState = GameState.Phase1;
-        SceneManager.LoadSceneAsync("Labyrinth");
-    }
-    
-    public void ReachedExit()
-    {
-        if (currentState != GameState.Phase1) return;
-        
-        currentState = GameState.Phase2;
-        SceneManager.LoadSceneAsync("Labyrinth");
+        currentState = GameState.Playing;
+        SceneManager.LoadScene("PCGTestScene");
     }
 
-    public void WinLevel()
+    public void WinGame()
     {
-        if (currentState != GameState.Phase2) return;
-        currentScore++;
+        if (currentState != GameState.Playing) return;
 
         currentState = GameState.Won;
-        SceneManager.LoadSceneAsync("WinScene");
+        SceneManager.LoadScene("WinScene");
     }
 
     public void LoseGame()
     {
-        if (currentState is not (GameState.Phase1 or GameState.Phase2)) return;
-        currentState = GameState.Lost;
-        
-        lastRunTime = _currentRunTime;
-        if (currentScore > highScore) highScore = currentScore;
+        if (currentState != GameState.Playing) return;
 
-        SceneManager.LoadSceneAsync("LoseScene");
+        currentState = GameState.Lost;
+        SceneManager.LoadScene("LoseScene");
     }
 }
+
+
