60f8d554c2be5e8aa5c1729a4e6994227f30d282
ValdemarWessing <iq62FM@student.aau.dk>
2025-05-01 19:17:23 +0200

    save highscore data

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
 using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
+[System.Serializable]
+public class HighScoreData
+{
+    public int highScore;
+}
 [Serializable]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    private string _dataPath;
 
     // ------------------ VARIABLES ------------------
     [Header("State")] public GameState currentState;
@@ -26,7 +33,6 @@ public class GameManager : MonoBehaviour
 
     private float _currentRunTime;
 
-
     // ------------------ METHODS ------------------
     private void Awake()
     {
@@ -36,6 +42,11 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else Destroy(gameObject);
+        
+        _dataPath = Application.persistentDataPath + "/Player_Data/";
+        Debug.Log(_dataPath);
+
+        LoadHighScoreFromJson();
     }
 
     private void Start()
@@ -95,11 +106,39 @@ public class GameManager : MonoBehaviour
     {
         if (currentState is not (GameState.Phase1 or GameState.Phase2)) return;
         currentState = GameState.Lost;
+        
 
         lastRunTime = _currentRunTime;
         if (currentScore > highScore) highScore = currentScore;
         currentScore = 0;
+        SaveHighScoreToJson();
 
         SceneManager.LoadSceneAsync("LoseScene");
+    }
+    public void SaveHighScoreToJson()
+    {
+        Directory.CreateDirectory(_dataPath);
+        Debug.Log("New directory created!");
+
+        HighScoreData highScoreData = new HighScoreData { highScore = highScore };
+        string json = JsonUtility.ToJson(highScoreData, true);
+
+        string filePath = Path.Combine(_dataPath, "highScore.json");
+        File.WriteAllText(filePath, json);
+        Debug.Log("High score saved to: " + filePath);
+    }
+    public void LoadHighScoreFromJson()
+    {
+        string filePath = Path.Combine(_dataPath, "highScore.json");
+        if (!File.Exists(filePath))
+        {
+            Debug.LogWarning("High score file does not exist!");
+            return;
+        }
+
+        string text = File.ReadAllText(filePath);
+        HighScoreData data = JsonUtility.FromJson<HighScoreData>(text);
+        highScore = data.highScore;
+        Debug.Log("Loaded high score: " + highScore);
     }
 }
